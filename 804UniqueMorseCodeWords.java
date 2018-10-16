f4ebd42bb9839e4274daffe7f9e295b4bcb60c6c
xandersu <xandersu666@gmail.com>
2018-10-16 21:10:48 +0800

    Create 804UniqueMorseCodeWords.java

---
+++ 804UniqueMorseCodeWords.java
@@ -0,0 +1,16 @@
+public class UniqueMorseCodeWords {
+
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] codes = { ".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        TreeSet<String> set = new TreeSet<>();
+        for (String word : words) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                String code = codes[word.charAt(i) - 'a'];
+                res.append(code);
+            }
+            set.add(res.toString());
+        }
+        return set.size();
+    }
+}
