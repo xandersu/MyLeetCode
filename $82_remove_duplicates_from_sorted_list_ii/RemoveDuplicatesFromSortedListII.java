abbcebb5022017392cad00272dc92513ea502ebb
xandersu <840595532@qq.com>
2020-04-27 16:43:35 +0800

    add 「LeetCode」 082  删除排序链表中的重复元素2

---
+++ $82_remove_duplicates_from_sorted_list_ii/RemoveDuplicatesFromSortedListII.java
@@ -0,0 +1,42 @@
+package $82_remove_duplicates_from_sorted_list_ii;
+
+/**
+ * @author: suxun
+ * @date: 2020/4/27 16:41
+ * @description: 82. 删除排序链表中的重复元素 II
+ * 给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字。
+ */
+public class RemoveDuplicatesFromSortedListII {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {
+        }
+
+        public ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        head = dummy;
+        while (head.next != null && head.next.next != null) {
+            if (head.next.val == head.next.next.val) {
+                int val = head.next.val;
+                while (head.next != null && head.next.val == val) {
+                    head.next = head.next.next;
+                }
+            } else {
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+}
